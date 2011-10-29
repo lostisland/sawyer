0150e9718b25e95e6cf7600644ff63d745d27e91
rick <technoweenie@gmail.com>
2011-10-28 22:58:32 -0600

    remove dead weight

--- lib/sawyer.rb
+++ lib/sawyer.rb
@@ -2,21 +2,7 @@ module Sawyer
   VERSION = "0.0.1"
 
   class Error < StandardError; end
-
-  class HttpError < Error
-    attr_reader :response
-
-    def initialize(response, message)
-      @response = response
-      super message
-    end
-  end
 end
 
 %w(
-  mime_type
-  relation
-  schema
-  resource
-  agent
 ).each { |f| require File.expand_path("../sawyer/#{f}", __FILE__) }
