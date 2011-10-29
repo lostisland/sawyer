10454e30a27a896ae703992af7f659501ffc3cf1
rick <technoweenie@gmail.com>
2011-10-28 23:27:21 -0600

    introduce a Relation::Map

--- lib/sawyer/relation.rb
+++ lib/sawyer/relation.rb
@@ -1,5 +1,31 @@
 module Sawyer
   class Relation
+    class Map
+      # Tracks the available next actions for a resource, and
+      # issues requests for them.
+      def initialize
+        @map = {}
+      end
+
+      # Adds a Relation to the map.
+      #
+      # rel - A Relation.
+      #
+      # Returns nothing.
+      def <<(rel)
+        @map[rel.name] = rel
+      end
+
+      # Gets the raw Relation by its name.
+      #
+      # key - The Symbol name of the Relation.
+      #
+      # Returns a Relation.
+      def [](key)
+        @map[key.to_sym]
+      end
+    end
+
     attr_reader :name,
       :href,
       :method
@@ -10,13 +36,12 @@ module Sawyer
     # index - The Hash mapping Relation names to the Hash Relation
     #         options.
     #
-    # Returns a Hash mapping Relation names to Relations.
+    # Returns a Relation::Map
     def self.from_links(index)
-      rels = {}
+      rels = Map.new
 
       index.each do |name, options|
-        rel = from_link name, options
-        rels[rel.name] = rel
+        rels << from_link(name, options)
       end
 
       rels
@@ -25,7 +50,7 @@ module Sawyer
     # Public: Builds a single Relation from the given options.  These are
     # usually taken from a `_links` property in a resource.
     #
-    # name    - The Symbol name of the relation.
+    # name    - The Symbol name of the Relation.
     # options - A Hash containing the other Relation properties.
     #           :_href   - The String URL of the next action's location.
     #           :_method - The optional String HTTP method.
@@ -35,16 +60,16 @@ module Sawyer
       new name, options[:_href], options[:_method]
     end
 
-    # A Relation represents an available next action for a resource.
+    # Public: A Relation represents an available next action for a resource.
     #
     # name   - The Symbol name of the relation.
     # href   - The String URL of the location of the next action.
     # method - The Symbol HTTP method.  Default: :get
     def initialize(name, href, method = nil)
-      @name   = name.to_sym
-      @href   = href.to_s
+      @name = name.to_sym
+      @href = href.to_s
 
-      if method.is_a?(String)
+      if method.is_a? String
         if method.size.zero?
           method = nil
         else
