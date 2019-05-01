37c9ad24a072984a2d44eb88caff01272a689fb4
rick olson <technoweenie@gmail.com>
2019-05-01 11:14:56 -0600

    Disable yaml tests in Ruby 2.5+ because of issues with Psych 3.0

--- test/helper.rb
+++ test/helper.rb
@@ -4,4 +4,17 @@ require File.expand_path('../../lib/sawyer', __FILE__)
 class Sawyer::TestCase < Minitest::Test
   def default_test
   end
+
+  def supports_yaml?
+    return true if ruby_version < yaml_disabled_version
+    ENV["SAWYER_YAML_ENABLED"] == "1"
+  end
+
+  def ruby_version
+    Gem::Version.new(RUBY_VERSION)
+  end
+
+  def yaml_disabled_version
+    Gem::Version.new("2.5.0")
+  end
 end
